11e0b2601aaefbc43cb11487b966042634860fd1
elisabedashvili-exceed <v.elisabedashvili.exceedteam@gmail.com>
2020-07-27 16:44:17 +0400

    update

--- src/App.js
+++ src/App.js
@@ -7,110 +7,86 @@ for (let j = 1; j <= 10000; j++) {
 	bigArray.push('Item No' + j);
 }
 
-const initialState = {
-	array: bigArray,
-	countedArray: [],
-	filteredArray: [],
-	searchPattern: [],
-	inputValue: '',
-	amount: 100,
-};
-
 const inputField = createRef();
 const barField = createRef();
-const loadMoreRef = createRef();
 
 class App extends Component {
-	state = initialState;
+	state = {
+		array: bigArray,
+		countedArray: [],
+		searchPattern: [],
+		inputValue: '',
+		amount: 100,
+		visible: false,
+	};
 
 	searchPattern = () => {
 		let { searchPattern } = this.state;
 		return new RegExp(searchPattern.map((e) => `(?=.*${e})`).join(''), 'i');
 	};
 
-	addload100ItemsListener = () => {
-		loadMoreRef.current.addEventListener('click', this.load100Items);
-	};
-	removeload100ItemsListener = () => {
-		loadMoreRef.current.removeEventListener('click', this.load100Items);
-	};
-	addloadAndFilterListener = () => {
-		loadMoreRef.current.addEventListener('click', this.loadAndFilter);
-	};
-	removeloadAndFilterListener = () => {
-		loadMoreRef.current.removeEventListener('click', this.loadAndFilter);
-	};
-
 	handleChange = (e) => {
-		let { array } = this.state;
+		let { array, amount } = this.state;
 		let value = e.target.value;
 		if (value.length) {
+			barField.current.scrollTop = 0;
 			this.setState(
 				{
 					inputValue: value,
-					amount: 100,
 					searchPattern: value.split(' '),
+					visible: true,
 				},
 				() => {
-					this.setState(
-						{
-							filteredArray: array.filter((item) =>
-								item.match(this.searchPattern())
-							),
-						},
-						() => {
-							this.filterByAmount();
-							this.removeload100ItemsListener();
-							this.addloadAndFilterListener();
-						}
+					let filteredArray = array.filter((item) =>
+						item.match(this.searchPattern())
 					);
+					this.setState({
+						countedArray: filteredArray.slice(0, amount),
+					});
 				}
 			);
 		} else {
-			this.setState(initialState);
+			this.setState({ inputValue: '', searchPattern: [], visible: false });
 		}
 	};
 
-	filterByAmount = () => {
-		let { filteredArray, amount } = this.state;
-		let countedArray = [];
-		for (let i = 0; i < Math.min(filteredArray.length, amount); i++) {
-			countedArray.push(filteredArray[i]);
+	handleFocus = (e) => {
+		let { inputValue, amount, array, countedArray, visible } = this.state;
+		this.handleChange(e);
+		if (!inputValue && document.activeElement === inputField.current) {
+			this.setState({ countedArray: array.slice(0, amount), visible: true });
 		}
-		this.setState({ countedArray: countedArray });
+		barField.current.scrollTop = 0;
 	};
 
-	loadMore = () => {
-		let { amount } = this.state;
-		this.setState({ amount: amount + 100 });
+	handleBlur = () => {
+		this.setState({ visible: false });
 	};
 
-	loadAndFilter = () => {
-		this.loadMore();
-		this.filterByAmount();
-	};
-
-	load100Items = () => {
-		let { countedArray, amount, array } = this.state;
-		if (countedArray.length > amount) {
+	handleScroll = () => {
+		let { amount, countedArray, array } = this.state;
+		let filteredArray = array.filter((item) =>
+			item.match(this.searchPattern())
+		);
+		if (
+			barField.current.scrollTop + barField.current.clientHeight + 500 >=
+			barField.current.scrollHeight
+		) {
 			this.setState({
-				countedArray: array.filter(
-					(e, i, arr) => arr.indexOf(e) < amount + 100
-				),
-			});
-		} else {
-			this.setState({
-				countedArray: array.filter((e, i, arr) => arr.indexOf(e) < amount),
+				countedArray: (filteredArray.length > 0
+					? filteredArray
+					: array
+				).slice(0, countedArray.length + amount),
 			});
 		}
 	};
 
 	render() {
-		let { inputValue, countedArray } = this.state;
-		// console.log(this.state)
-		
+		let { countedArray, visible } = this.state;
+		console.log(this.state);
+
 		return (
-			<React.Fragment>
+			<div className="container">
 				<TextField
 					inputRef={inputField}
 					className="regInputField"
@@ -118,67 +94,32 @@ class App extends Component {
 					size="small"
 					variant="outlined"
 					onChange={this.handleChange}
-					onClick={() => {
-						if (
-							!inputValue &&
-							document.activeElement === inputField.current
-						) {
-							this.removeloadAndFilterListener();
-							this.addload100ItemsListener();
-							this.load100Items();
-						}
-					}}
-					onFocus={
-						this.handleChange
-					}
+					onFocus={this.handleFocus}
 					onBlur={() => {
 						setTimeout(() => {
-							this.setState(initialState);
+							this.handleBlur();
 						}, 100);
 					}}
 				/>
 				<div
-					className={
-						countedArray.length >= 20
-							? 'scrollBar'
-							: !inputValue || countedArray.length === 0
-							? 'hide'
-							: 'barShow'
-					}
-					id={'listbar'}
+					className={visible ? 'show' : 'hide'}
+					id="listbar"
 					ref={barField}
-					onScroll={() => {
-						if (
-							barField.current.scrollTop +
-								barField.current.clientHeight +
-								500 >=
-							barField.current.scrollHeight
-						) {
-							loadMoreRef.current.click();
-						}
-					}}
+					onScroll={this.handleScroll}
 				>
 					{countedArray.map((item, i) => (
 						<div
 							key={i}
 							onClick={() => {
-								this.setState(initialState);
+								this.setState({ visible: false });
 								inputField.current.value = item;
 							}}
 						>
 							{item}
 						</div>
 					))}
-					<button
-						ref={loadMoreRef}
-						onClick={this.loadMore}
-						style={{ display: 'none' }}
-						id={'button'}
-					>
-						Load More
-					</button>
 				</div>
-			</React.Fragment>
+			</div>
 		);
 	}
 }
